d5314380c6237101331bc538307f7fa69885ca63
Matt Shore <mazzamds@gmail.com>
2020-11-13 00:59:33 +0000

    Removed SVG issue for now.

--- components/Header.js
+++ components/Header.js
@@ -8,15 +8,7 @@ const vh = Dimensions.get('window').height;
 export default function Header({ text, homeFunction }) {
     return (
         <View style={styles.header} onPress={homeFunction}>
-            <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='24'
-                height='24'
-                fillRule='evenodd'
-                clipRule='evenodd'
-             style={styles.headerHomeButton}>
-                <path d='M22 11.414V24H2V11.414L.707 12.707 0 12 12 0l12 12-.707.707L22 11.414zM16 23h5V10.414l-9-9-9 9V23h5v-9h8v9zm-1-7.889H9v7.778h6v-7.778z'></path>
-            </svg>
+          
             <Text style={styles.headerText}>{text}</Text>
         </View>
     )
@@ -37,8 +29,5 @@ const styles = StyleSheet.create({
         color: 'white',
         fontWeight: 'bold',
         fontSize: 25
-    },
-    headerHomeButton: {
-       alignItems: 'right'
     }
 })
