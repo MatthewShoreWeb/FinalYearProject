4fd8c15acbb5a9cd429244a162ee76a79d38e0ae
Matt Shore <mazzamds@gmail.com>
2021-05-15 20:50:23 +0100

    Bug fix.

--- components/TestFeedback.js
+++ components/TestFeedback.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { StyleSheet, View, Text, ScrollView } from 'react-native';
 import Panel from './PanelDropDown'
 
@@ -78,7 +78,7 @@ export default function TestFeedback({ data, display }) {
     return (
         <ScrollView style={styles.container}>
             <View style={styles.topTextContainer}>
-                <Text style={styles.text}>{'You scored 60%, well done. Keep practicing your mistakes.'}</Text>
+                <Text style={styles.text}>{topMessage}</Text>
             </View>
         <Panel header={'Question 1 was ' + generateText(data[0], true)} text={generateText(data[0])} display={display}></Panel>
         <Panel header={'Question 2 was ' + generateText(data[1], true)} text={generateText(data[1])} display={display}></Panel>
